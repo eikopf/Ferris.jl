befdd0e5feacc862eafbef8ed92ddc098b2fb18a
Oliver Wooding <oliverwooding@icloud.com>
2024-05-30 17:58:51 +0100

    added and/or (+ unicode infix synonyms) to module export list

--- src/Ferris.jl
+++ src/Ferris.jl
@@ -3,14 +3,14 @@ include("Core.jl")
 include("Results.jl")
 include("Options.jl")
 
-import .Core: unwrap, flatten, lift
+import .Core: unwrap, flatten, lift, and, or, ∨, ∧
 using .Results, .Options
 
 import MLStyle: @match, @λ
 
 export Ok, Err, Result,
   None, Some, Option, option,
-  unwrap, flatten, lift,
+  unwrap, flatten, lift, and, or, ∨, ∧,
   @match, @λ
 
 end
